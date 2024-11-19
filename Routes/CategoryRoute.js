0e71f2226dffd264d80b05c9f7779774812553a0
Tavera15 <joaquinramirez97@yahoo.com>
2024-11-19 00:58:37 -0600

    Added token verification on category routes

--- Routes/CategoryRoute.js
+++ Routes/CategoryRoute.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const CategoryController = require("../Controllers/CategoryController");
+const verifyToken = require("../Middleware/Auth");
 
 router.get("/:name?", CategoryController.GetCategories);
-router.post("/CreateCategory", (req, res) => CategoryController.CreateCategory(req, res));
-router.put("/UpdateCategory/:name", (req, res) => CategoryController.UpdateCategory(req, res));
-router.delete("/DeleteCategory/:name", CategoryController.DeleteCategory);
+router.post("/CreateCategory", verifyToken, (req, res) => CategoryController.CreateCategory(req, res));
+router.put("/UpdateCategory/:name", verifyToken, (req, res) => CategoryController.UpdateCategory(req, res));
+router.delete("/DeleteCategory/:name", verifyToken, CategoryController.DeleteCategory);
 
 module.exports = router;
